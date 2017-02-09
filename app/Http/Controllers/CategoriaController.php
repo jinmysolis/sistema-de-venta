98e4dfdd68984bc69d83ebce222ee040950585cf
jinmy <solisjinmy@gmail.com>
2017-02-08 22:04:24 -0430

    listado y paginacion categorias

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -30,7 +30,7 @@ class CategoriaController extends Controller
                     $categorias=DB::table('categoria')->where('nombre','LIKE','%'.$query.'%')
                             ->where('condicion','=','1')
                             ->orderBy('idcategoria','desc')
-                            ->paginate(7);
+                            ->paginate(2);
                     return view('almacen.categoria.index',["categorias"=>$categorias,"searchText"=>$query
                             ]);
                 }
--- resources/views/almacen/categoria/index.blade.php
+++ resources/views/almacen/categoria/index.blade.php
@@ -4,7 +4,49 @@
 
 @section('content')
 
-<h3>index</h3>
+<div class="row">
+    <div   class="col-lg-8 col-md-8 col-sm-8 col-xs-12"
+           <h3>Listado de categorias <a href="categoria.create" class="btn btn-primary">Nuevo</a></h3>
+          @include('almacen.categoria.search')
+</div>
 
+    <div class="row">
+    <div   class="col-lg-12 col-md-12 col-sm-12 col-xs-12"
+           <div class="table-responsive">
+             <table class="table table-hover">
+                <thead>
+                  
+                    <th>ID</th>
+                    <th>NOMBRE</th>
+                    <th>DESCRIPCION</th>
+                    <th>OPERACIONES</th>
+                   
+                </thead>
+                
+                @foreach($categorias as $categoria)     
+                  <tr class="success">
+                      <td>{{$categoria->idcategoria}}</td>
+                      <td>{{$categoria->nombre}}</td>
+                      <td>{{$categoria->descripcion}}</td>
+                     
+                          <td > 
+                          <a href="" class="btn btn-danger glyphicon glyphicon-refresh"></a> 
+                          <a href="" onclick="return confirm('Seguro desea eliminar')"class="btn btn-warning glyphicon glyphicon-trash"></a>
+                         </td>
+                          
+                     
+                   </tr>
 
+               
+               @endforeach
+              </table>
+               
+          </div>
+          
+          
+  </div>
+        <div class="text-center">
+           {!! $categorias->setPath('')->render()!!} 
+        </div>
+ 
 @endsection
